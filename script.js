d8000ff924bb6978b627f5b07888caf0b415fa15
genaro-cravero <nicocraveroo@gmail.com>
2023-06-14 18:33:55 -0300

    Finished

--- script.js
+++ script.js
@@ -1,38 +1,90 @@
-let form = document.querySelector('#quizz1');
-let formNum = 1;
+let button = document.querySelector("button");
 
-form.addEventListener('submit', function(e)
-{
-    e.preventDefault();
+let allForms = document.querySelectorAll('form');
+let allFormsArray = Array.from(allForms);
+let currentForm = 0;
 
-    let usuario = document.getElementById("Nombre").value;
+let allFinals = document.querySelectorAll('h1');
+let allFinalsArray = Array.from(allFinals);
 
-    let genero = document.querySelector('input[name="genero"]:checked');
-    let generoValue ;
-    if(genero){
-        generoValue = genero.value;
-        console.log("Género seleccionado:", generoValue);
-    } else {
 
-        console.log("No se seleccionó ningún género");
+let nombre,claseValue,q2, final;
+
+allFormsArray.forEach((form) => {
+    form.style.display = "none";
+});
+allFinalsArray.forEach((final) => {
+    final.style.display = "none";
+});
+
+allFormsArray[currentForm].style.display = "flex";
+
+button.addEventListener("click", () => {
+    if(currentForm >= 0 && currentForm <= 6)
+    {
+
+        allFormsArray[currentForm].style.display = "none";
     }
     
-    console.log("Nombre:", usuario);
-  
-    // Aquí puedes hacer lo que quieras con los datos
-    
-    // se reinicio
-    mostrarFormulario(formNum)
-    form.reset();
-})
+    if (currentForm === 0) {
+        nombre = document.querySelector("#Nombre").value;
+        claseValue = document.querySelector('input[name="clase"]:checked').value;
 
-function mostrarFormulario(formulario) {
+        currentForm++;
+        allFormsArray[currentForm].style.display = "flex";
 
-    // const formActual = document.getElementById(`quizz${formulario}`);
-    // const formSiguiente = document.getElementById(`quizz${formulario + 1}`);
+    }else if(currentForm === 1){
+
+        currentForm++;
+        allFormsArray[currentForm].style.display = "flex";
+
+    }else if(currentForm === 2){
+        q2 = document.querySelector('input[name="q2"]:checked').value;
+        q2 = parseInt(q2);
     
-    // formActual.style.display = "block";
-    // formSiguiente.style.display = "block";
-    // formNum++;
-    // form = document.querySelector(`#quizz${formNum}`);
-  }
+        SwitchQuizz(q2);
+                
+        allFormsArray[currentForm].style.display = "flex";
+    }else if(currentForm >= 3 && currentForm <= 6 ){
+
+        final = document.querySelector(`input[name="q${currentForm}"]:checked`).value;
+
+        ShowFinal(final);
+    }
+    
+
+
+    if(currentForm === 2){
+        document.querySelector("#nombre").innerHTML = nombre;
+    }
+    
+});
+
+const SwitchQuizz = (quizz) => {
+    switch (quizz)
+    {
+        case 0:
+            currentForm++;
+            break;
+        case 1:
+            currentForm += 2;
+            break;
+        case 2:
+            currentForm += 3;
+            break;
+        case 3:
+            currentForm += 4;
+            break;
+    }
+}
+
+const ShowFinal = (final) => {
+    button.style.display = "none";
+    if(claseValue === "duende"){
+        document.querySelector("#MuerteSegura").style.display = "flex";
+    }
+    else{
+
+        document.querySelector(`#${final}`).style.display = "flex";
+    }
+}
